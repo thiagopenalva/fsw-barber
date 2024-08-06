338ea6758f9384287ef7308407575f47543d33f0
Thiago Penalva <thiagopenalva@hotmail.com>
2024-08-06 13:29:39 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/Header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
-  return <Button>Test</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text xl font-bold">Olá, Thiago!</h2>
+        <p>Terça-feira, 06 de agosto</p>
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça a sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/Banner-01.png"
+            alt="Agende nos melhores com FSW Barber"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
