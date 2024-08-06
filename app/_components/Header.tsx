122a7f35e2df22eb3d36e7008f2bf36b2578635c
Thiago Penalva <thiagopenalva@hotmail.com>
2024-08-06 20:26:18 -0300

    feat: add BarbershopItem component

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent } from "./ui/card"
 import { Button } from "./ui/button"
 import { MenuIcon } from "lucide-react"
 
-const Header = () => {
+const Header = async () => {
   return (
     <div>
       <Card>
