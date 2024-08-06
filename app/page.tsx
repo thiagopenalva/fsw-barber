db8c06bc8f2e6dbc8370565e01d6843279fb2465
Thiago Penalva <thiagopenalva@hotmail.com>
2024-08-06 20:50:48 -0300

    feat: add quick search

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,11 @@ import BarbershopItem from "./_components/Barbershop-item"
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({})
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  })
 
   return (
     <div>
@@ -24,6 +29,30 @@ const Home = async () => {
             <SearchIcon />
           </Button>
         </div>
+
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          <Button className="gap-2" variant={"secondary"}>
+            <Image src="/cabelo.svg" width={16} height={16} alt="cabelo" />
+            Cabelo
+          </Button>
+          <Button className="gap-2" variant={"secondary"}>
+            <Image src="/barba.svg" width={16} height={16} alt="cabelo" />
+            Barba
+          </Button>
+          <Button className="gap-2" variant={"secondary"}>
+            <Image src="/acabamento.svg" width={16} height={16} alt="cabelo" />
+            Acabamento
+          </Button>
+          <Button className="gap-2" variant={"secondary"}>
+            <Image src="/acabamento.svg" width={16} height={16} alt="cabelo" />
+            Acabamento
+          </Button>
+          <Button className="gap-2" variant={"secondary"}>
+            <Image src="/acabamento.svg" width={16} height={16} alt="cabelo" />
+            Acabamento
+          </Button>
+        </div>
+
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             src="/Banner-01.png"
@@ -62,7 +91,25 @@ const Home = async () => {
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+        <div className="flex gap-2 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
+      <footer>
+        <Card>
+          <CardContent className="px-5 py-6">
+            <p className="text-sm text-gray-400">
+              © 2023 Copyright <span className="text-bold">FSW Barber</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   )
 }
