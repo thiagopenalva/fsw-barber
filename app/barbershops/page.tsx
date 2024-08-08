c2aae774c534e1a62efa27dda52beb2f4c1147d4
Thiago Penalva <thiagopenalva@hotmail.com>
2024-08-08 19:30:45 -0300

    feat: ensure user can search for barbershops

---
+++ app/barbershops/page.tsx
@@ -0,0 +1,41 @@
+import BarbershopItem from "../_components/Barbershop-item"
+import Header from "../_components/Header"
+import Search from "../_components/search"
+import { db } from "../_lib/prisma"
+
+interface BarbershopPageProps {
+  searchParams: {
+    search: string
+  }
+}
+
+const BarbershopsPage = async ({ searchParams }: BarbershopPageProps) => {
+  const barbershops = await db.barbershop.findMany({
+    where: {
+      name: {
+        contains: searchParams?.search,
+        mode: "insensitive",
+      },
+    },
+  })
+  return (
+    <div>
+      <Header />
+      <div className="my-6 px-5">
+        <Search />
+      </div>
+      <div className="px-5">
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Resultados para {searchParams?.search}
+        </h2>
+        <div className="grid grid-cols-2 gap-4">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BarbershopsPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import { SearchIcon } from "lucide-react"
 import Header from "./_components/Header"
 import { Button } from "./_components/ui/button"
-import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/Barbershop-item"
 import { quickSearchOptions } from "./_constants/search"
 import BookingItems from "./_components/Booking-item"
+import Search from "./_components/search"
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({})
@@ -22,11 +21,8 @@ const Home = async () => {
       <div className="p-5">
         <h2 className="text xl font-bold">Olá, Thiago!</h2>
         <p>Terça-feira, 06 de agosto</p>
-        <div className="mt-6 flex items-center gap-2">
-          <Input placeholder="Faça a sua busca..." />
-          <Button>
-            <SearchIcon />
-          </Button>
+        <div className="mt-6">
+          <Search />
         </div>
 
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
